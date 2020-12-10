cb66320dfa1c3e427e75a4caa715d670aab023fb
JP Ungaretti <hello@jungaretti.com>
2020-12-10 16:55:23 -0600

    Clean up day_01.rs

--- src/day_01.rs
+++ src/day_01.rs
@@ -15,25 +15,25 @@ fn load_numbers_from_file(file_path: &str) -> Vec<i32> {
         .collect();
 }
 
-fn two_sum(numbers: &[i32], target: i32) -> Option<i32> {
-    let mut pairs: HashSet<i32> = HashSet::new();
-    for number in numbers {
+fn two_sum(target: i32, numbers: &Vec<i32>, starting_index: usize) -> Option<i32> {
+    let mut complements: HashSet<i32> = HashSet::new();
+    for number in &numbers[starting_index..] {
         let needed = target - number;
-        if pairs.contains(&needed) {
+        if complements.contains(&needed) {
             return Some(needed * number);
         }
-        pairs.insert(*number);
+        complements.insert(*number);
     }
     return None;
 }
 
-fn three_sum(numbers: &Vec<i32>, target: i32) -> Option<i32> {
+fn three_sum(target: i32, numbers: &Vec<i32>) -> Option<i32> {
     let mut sorted_numbers = numbers.to_owned();
     sorted_numbers.sort();
 
-    for (position, number) in sorted_numbers.iter().enumerate() {
+    for (pos, number) in sorted_numbers.iter().enumerate() {
         let needed = target - number;
-        if let Some(two_sum) = two_sum(&sorted_numbers[position + 1..], needed) {
+        if let Some(two_sum) = two_sum(needed, &sorted_numbers, pos + 1) {
             return Some(number * two_sum);
         }
     }
@@ -42,10 +42,10 @@ fn three_sum(numbers: &Vec<i32>, target: i32) -> Option<i32> {
 
 pub fn day_01_challenge_1() -> i32 {
     let numbers = load_numbers_from_file(DATA_FILE_PATH);
-    return two_sum(&numbers, 2020).unwrap();
+    return two_sum(2020, &numbers, 0).unwrap();
 }
 
 pub fn day_01_challenge_2() -> i32 {
     let numbers = load_numbers_from_file(DATA_FILE_PATH);
-    return three_sum(&numbers, 2020).unwrap();
+    return three_sum(2020, &numbers).unwrap();
 }
