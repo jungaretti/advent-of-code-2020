2ea98592c9b2eb86942a497fb53e051a79f199c3
JP Ungaretti <hello@jungaretti.com>
2020-12-18 17:20:31 -0600

    Move logic to toboggan_travel

--- src/day_03.rs
+++ src/day_03.rs
@@ -4,9 +4,6 @@ use std::io::BufReader;
 
 const DATA_FILE_PATH: &str = "./data/day03.txt";
 
-const SLOPE_X: usize = 3;
-const SLOPE_Y: usize = 1;
-
 #[derive(PartialEq)]
 enum ForestEntity {
     Square,
@@ -31,24 +28,26 @@ fn load_forest_from_file(file_path: &str) -> Vec<Vec<ForestEntity>> {
         .collect();
 }
 
-pub fn day_03_challenge_1() -> i32 {
-    let forest = load_forest_from_file(DATA_FILE_PATH);
-
+fn toboggan_travel(forest: &Vec<Vec<ForestEntity>>, slope_x: usize, slope_y: usize) -> i32 {
     let mut result = 0;
+
     let mut pos_x = 0;
     let mut pos_y = 0;
     while pos_y < forest.len() {
         let row = &forest[pos_y];
         let entity = &row[pos_x];
-
         if *entity == ForestEntity::Tree {
             result += 1
         }
-
-        pos_x += SLOPE_X;
+        pos_x += slope_x;
         pos_x %= row.len();
-        pos_y += SLOPE_Y;
+        pos_y += slope_y;
     }
 
     return result;
+}
+
+pub fn day_03_challenge_1() -> i32 {
+    let forest = load_forest_from_file(DATA_FILE_PATH);
+    return toboggan_travel(&forest, 3, 1);
 }
